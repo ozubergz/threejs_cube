0f7b3c2e9fb3a202407ba0685c19b7c1cb133e60
ozubergz <jmin499@gmail.com>
2020-09-03 23:56:28 -0400

    set camera

--- components/primitive.js
+++ components/primitive.js
@@ -2,3 +2,11 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xAAAAAA);
+
+const fov = 40;
+const aspect = 2; //canvas default
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 120;
+
