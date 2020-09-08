7fa68ecdbf99f3c189457dcc014bef46a52f0dc3
ozubergz <jmin499@gmail.com>
2020-09-07 22:14:41 -0400

    parametric geometry

--- components/primitives.js
+++ components/primitives.js
@@ -98,8 +98,9 @@ function addSolidGeometry(x, y, geometry) {
 {
     const radius = 7;
     addSolidGeometry(2, 2, new THREE.DodecahedronBufferGeometry(radius));
-  }
-  {
+}
+
+{
     const shape = new THREE.Shape();
     const x = -2.5;
     const y = -5;
@@ -112,31 +113,63 @@ function addSolidGeometry(x, y, geometry) {
     shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
 
     const extrudeSettings = {
-      steps: 2,
-      depth: 2,
-      bevelEnabled: true,
-      bevelThickness: 1,
-      bevelSize: 1,
-      bevelSegments: 2,
+        steps: 2,
+        depth: 2,
+        bevelEnabled: true,
+        bevelThickness: 1,
+        bevelSize: 1,
+        bevelSegments: 2,
     };
 
     addSolidGeometry(-2, 1, new THREE.ExtrudeBufferGeometry(shape, extrudeSettings));
-  }
-  {
+}
+
+{
     const radius = 7;
     addSolidGeometry(-1, 1, new THREE.IcosahedronBufferGeometry(radius));
-  }
-  {
+}
+
+{
     const points = [];
     for (let i = 0; i < 10; ++i) {
-      points.push(new THREE.Vector2(Math.sin(i * 0.2) * 3 + 3, (i - 5) * .8));
+        points.push(new THREE.Vector2(Math.sin(i * 0.2) * 3 + 3, (i - 5) * .8));
     }
     addSolidGeometry(0, 1, new THREE.LatheBufferGeometry(points));
-  }
-  {
+}
+
+{
     const radius = 7;
     addSolidGeometry(1, 1, new THREE.OctahedronBufferGeometry(radius));
+}
+{
+
+function klein(v, u, target) {
+    u *= Math.PI;
+    v *= 2 * Math.PI;
+    u = u * 2;
+
+    let x;
+    let z;
+
+    if (u < Math.PI) {
+        x = 3 * Math.cos(u) * (1 + Math.sin(u)) + (2 * (1 - Math.cos(u) / 2)) * Math.cos(u) * Math.cos(v);
+        z = -8 * Math.sin(u) - 2 * (1 - Math.cos(u) / 2) * Math.sin(u) * Math.cos(v);
+    } else {
+        x = 3 * Math.cos(u) * (1 + Math.sin(u)) + (2 * (1 - Math.cos(u) / 2)) * Math.cos(v + Math.PI);
+        z = -8 * Math.sin(u);
+    }
+
+    const y = -2 * (1 - Math.cos(u) / 2) * Math.sin(v);
+
+    target.set(x, y, z).multiplyScalar(0.75);
   }
+
+  const slices = 25;
+  const stacks = 25;
+  addSolidGeometry(2, 1, new THREE.ParametricBufferGeometry(klein, slices, stacks));
+}
+
+
 
 function resizeRendererToDisplaySize(renderer) {
 	const canvas = renderer.domElement;
