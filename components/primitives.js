c95942f476e0b705a474d85d621c7c4441039179
ozubergz <jmin499@gmail.com>
2020-09-07 22:11:28 -0400

    added more geometry

--- components/primitives.js
+++ components/primitives.js
@@ -95,6 +95,49 @@ function addSolidGeometry(x, y, geometry) {
     addSolidGeometry(1, 2, new THREE.CylinderBufferGeometry(radiusTop, radiusBottom, height, radialSegments));
 }
 
+{
+    const radius = 7;
+    addSolidGeometry(2, 2, new THREE.DodecahedronBufferGeometry(radius));
+  }
+  {
+    const shape = new THREE.Shape();
+    const x = -2.5;
+    const y = -5;
+    shape.moveTo(x + 2.5, y + 2.5);
+    shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+    shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+    shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+    shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+    shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+    shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+
+    const extrudeSettings = {
+      steps: 2,
+      depth: 2,
+      bevelEnabled: true,
+      bevelThickness: 1,
+      bevelSize: 1,
+      bevelSegments: 2,
+    };
+
+    addSolidGeometry(-2, 1, new THREE.ExtrudeBufferGeometry(shape, extrudeSettings));
+  }
+  {
+    const radius = 7;
+    addSolidGeometry(-1, 1, new THREE.IcosahedronBufferGeometry(radius));
+  }
+  {
+    const points = [];
+    for (let i = 0; i < 10; ++i) {
+      points.push(new THREE.Vector2(Math.sin(i * 0.2) * 3 + 3, (i - 5) * .8));
+    }
+    addSolidGeometry(0, 1, new THREE.LatheBufferGeometry(points));
+  }
+  {
+    const radius = 7;
+    addSolidGeometry(1, 1, new THREE.OctahedronBufferGeometry(radius));
+  }
+
 function resizeRendererToDisplaySize(renderer) {
 	const canvas = renderer.domElement;
 	const pixelRatio = window.devicePixelRatio;
