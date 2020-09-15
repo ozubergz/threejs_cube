2f5e6f47d259a6b1bbd13c0038ca43e9efee999b
ozubergz <jmin499@gmail.com>
2020-09-14 23:55:32 -0400

    init and animate function

--- components/spinning.js
+++ components/spinning.js
@@ -10,5 +10,14 @@ function init() {
     camera.position.z = 5;
 
     renderer = new THREE.WebGL1Renderer();
-    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 }
+
+function animate() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+init();
+
