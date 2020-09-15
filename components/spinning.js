23b9ac5dbf7409395d523c207cdabd39d806a96a
ozubergz <jmin499@gmail.com>
2020-09-15 00:13:30 -0400

    spinning cube

--- components/spinning.js
+++ components/spinning.js
@@ -1,13 +1,22 @@
 import * as THREE from 'three';
 
-let scene, camera, renderer;
+let scene, camera, renderer, cube;
+
+function createCube() {
+    let geometry = new THREE.BoxGeometry();
+    let material = new THREE.MeshBasicMaterial({color: 0x00a1cb});
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+}
 
 function init() {
     // set up environment
     scene = new THREE.Scene();
     
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / innerHeight, 1, 1000);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
+
+    createCube();
 
     renderer = new THREE.WebGL1Renderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -15,9 +24,13 @@ function init() {
 }
 
 function animate() {
+
+    cube.rotation.y += 0.01;
+
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
 
 init();
+animate();
 
