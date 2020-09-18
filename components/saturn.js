2cfbb3086ffa8d908cc1cc76654ae3917d07d111
ozubergz <jmin499@gmail.com>
2020-09-18 19:41:37 -0400

    refactor

--- components/saturn.js
+++ components/saturn.js
@@ -3,21 +3,19 @@ import * as THREE from 'three';
 let scene, 
     camera, 
     renderer, 
-    sphere, 
-    torus, 
     ADD = 0.01;
 
 function createSphere() {
     let geometry = new THREE.SphereGeometry(1, 30, 30);
     let material = new THREE.MeshBasicMaterial({color: 0xaa4a30});
-    sphere = new THREE.Mesh(geometry, material);
+    let sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
 }
 
 function createTorus(radius, tube, color) {
     let geometry = new THREE.TorusGeometry(radius, tube, 2, 50);
     let material = new THREE.MeshBasicMaterial({color});
-    torus = new THREE.Mesh(geometry, material);
+    let torus = new THREE.Mesh(geometry, material);
     
     torus.rotation.x = 1.7
     torus.rotation.y = 0.5;
