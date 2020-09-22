a023070566363ac0d789186be4d013f763daf900
ozubergz <jmin499@gmail.com>
2020-09-21 22:58:13 -0400

    bloom effect

--- components/earth.js
+++ components/earth.js
@@ -1,13 +1,19 @@
 import * as THREE from 'three';
+// import { AddEquation } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import Earth from '../public/Earth.glb.gltf';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 
+
+import Earth from '../public/Earth.glb.gltf';
 
 let scene, 
     camera, 
     renderer,
     loader,
     model,
+    composer,
     ADD = 0.01;
 
 function load3dModel() {
@@ -38,13 +44,28 @@ function init() {
     renderer = new THREE.WebGL1Renderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    
+    postProcessing();
+}
+
+function postProcessing() {
+    // add bloom effect
+    composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+
+    let bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), .5, 0, 0.1);
+    composer.addPass(bloomPass);
 }
 
 function animate() {
-    if(model) model.rotation.y += ADD;
+    if(model) {
+        model.rotation.y += ADD;
+    }
 
-    renderer.render(scene, camera);
+    // renderer.render(scene, camera);
     requestAnimationFrame(animate);
+
+    composer.render();
 }
 
 init();
