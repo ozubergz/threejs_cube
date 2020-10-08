2c8cc7bb216510fa47eb910b4839fb4285fc9f45
James Min <jmin499@gmail.com>
2020-10-08 16:09:46 -0400

    created 2nd sphere

--- components/orbit.js
+++ components/orbit.js
@@ -2,34 +2,40 @@ import * as THREE from 'three';
 
 let scene, camera, renderer;
 
-const createSphere = (size) => {
+const createSphere = (size, x, y) => {
     const geometry = new THREE.SphereGeometry(size, 20, 20);
     const material = new THREE.MeshBasicMaterial({color: 0xeeeeee});
     const sphere = new THREE.Mesh(geometry, material);
+    
+    sphere.position.x = x
+    sphere.position.y = y
+
     scene.add(sphere);
 }
 
 const createEllipse = () => {
     let curve = new THREE.EllipseCurve(
-        0,  0,            // ax, aY
-        2, 2,           // xRadius, yRadius
-        0,  2 * Math.PI,  // aStartAngle, aEndAngle
-        false,            // aClockwise
-        0                 // aRotation
+        0, 0,            // ax, aY
+        2, 2,            // xRadius, yRadius
+        0, 2 * Math.PI,  // aStartAngle, aEndAngle
+        false,           // aClockwise
+        0                // aRotation
     );
     
     let points = curve.getPoints( 50 );
     let geometry = new THREE.BufferGeometry().setFromPoints( points );
     
-    let material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
+    let material = new THREE.LineBasicMaterial( { color : 0x7579e7 } );
     
     // Create the final object to add to the scene
     let ellipse = new THREE.Line( geometry, material );
     ellipse.rotation.x = 5
     ellipse.rotation.y = .2
     
-    scene.add(ellipse)
+    scene.add(ellipse);
 }
+
+
 
 const init = () => {
     scene = new THREE.Scene();
@@ -38,7 +44,8 @@ const init = () => {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
     camera.position.z =5;
 
-    createSphere(1);
+    createSphere(1, 0, 0);
+    createSphere(.5, 2, 0);
     createEllipse();
     
     renderer = new THREE.WebGL1Renderer();
