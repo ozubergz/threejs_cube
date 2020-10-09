1b8067ece8bc33636d78c365f3c24c038150b2db
James Min <jmin499@gmail.com>
2020-10-08 21:16:10 -0400

    remove ellipse

--- components/orbit.js
+++ components/orbit.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 
-let scene, camera, renderer, sun, mercury;
+let scene, camera, renderer, sun, mercury, curve;
 
 const createSun = () => {
-    const geometry = new THREE.SphereGeometry(1, 30, 30);
+    const geometry = new THREE.SphereGeometry(0.4, 30, 30);
     const material = new THREE.MeshBasicMaterial({color: 0xeeeeee});
     sun = new THREE.Mesh(geometry, material);
 
@@ -11,7 +11,7 @@ const createSun = () => {
 }
 
 const createMercury = () => {
-    const geometry = new THREE.SphereGeometry(.2, 30, 30);
+    const geometry = new THREE.SphereGeometry(0.2, 30, 30);
     const material = new THREE.MeshBasicMaterial({color: 0xeeeeee});
     mercury = new THREE.Mesh(geometry, material);
 
@@ -19,23 +19,34 @@ const createMercury = () => {
 }
 
 const createEllipse = () => {
-    let curve = new THREE.EllipseCurve(
-        0, 0,            // ax, aY
-        2, 2,            // xRadius, yRadius
-        0, 2 * Math.PI,  // aStartAngle, aEndAngle
-        false,           // aClockwise
-        0                // aRotation
-    );
-    
-    let points = curve.getPoints( 50 );
-    let geometry = new THREE.BufferGeometry().setFromPoints( points );
-    
+    // curve = new THREE.EllipseCurve(
+    //     0, 0,            // ax, aY
+    //     2, 2,            // xRadius, yRadius
+    //     0, 2 * Math.PI,  // aStartAngle, aEndAngle
+    //     false,           // aClockwise
+    //     0                // aRotation
+    // );
+
+    curve = new THREE.CatmullRomCurve3( [
+        // new THREE.Vector3(-10, 0, 10),
+        new THREE.Vector3( -1, 0, 2),
+        
+        // new THREE.Vector3( -5, 5, 5 ),
+        new THREE.Vector3( 0, 0, 0 ),
+        new THREE.Vector3( 1, -1, 2),
+        // new THREE.Vector3( 10, 0, 10 )
+    ]);
+
+    curve.closed = true
+
+    let points = curve.getPoints(50);
+    let geometry = new THREE.Geometry().setFromPoints(points)
     let material = new THREE.LineBasicMaterial( { color : 0x7579e7 } );
     
     // Create the final object to add to the scene
     let ellipse = new THREE.Line( geometry, material );
-    ellipse.rotation.x = 5
-    ellipse.rotation.y = .2
+    // ellipse.rotation.x = 5
+    // ellipse.rotation.y = .2
     
     scene.add(ellipse);
 }
@@ -48,8 +59,8 @@ const init = () => {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
 
-    createSun();
-    createMercury();
+    // createSun();
+    // createMercury();
     createEllipse();
     
     renderer = new THREE.WebGL1Renderer();
@@ -64,9 +75,14 @@ const animate = () => {
     requestAnimationFrame(animate);
 
     speed += 0.01;
+    // let pos = curve.getPoint(speed)
+    
+    // mercury.position.copy(pos)
 
-    mercury.position.x = Math.cos(speed) * radius;
-    mercury.position.z = Math.sin(speed) * radius; // These to strings make it work
+    // mercury.position.set(pos.x, pos.y, pos.z);
+
+    // mercury.position.x = Math.cos(speed) * radius;
+    // mercury.position.z = Math.sin(speed) * radius; // These to strings make it work
     
     renderer.render(scene, camera);
 }
